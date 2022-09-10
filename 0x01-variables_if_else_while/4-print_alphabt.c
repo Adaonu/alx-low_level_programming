3fb2f09c8f6b88d7036bcd0b1dafc757947f18a9
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-10 09:18:09 -0700

    w

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print alphabet lowercase expect q and e
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	if !(letter > 'e') && (letter > 'q')
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+
+
+}

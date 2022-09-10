28e279b8e3da7e81b6417a2523279057fc987dca
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-10 08:53:02 -0700

    w

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * print alphabet in lowercase and uppercase
+ *
+ * Return 0  alwats (succes)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{ 
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}

cf306cfe11acec9ec09b5783ddcb25bb80ff8529
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-10 07:29:09 -0700

    S

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,21 @@
  */
 int main(void)
 {
-	int n; 
-	
+	int n;
 	srand(time(0));
-	n = rand() -RAND_MAX /2;
-		if ( n > 0 ) {
-			printf( "%d is postive\n", n);
+	n = rand()-RAND_MAX/2;
+		if (n > 0)
+	       	{
+			printf("%d is postive\n", n);
+		
+	
+		else if (n == 0)
+	       	{
+			printf("%d is postive\n", n);
 		}
-		else if ( n == 0) {
-			printf( "%d is postive\n", n);
-		}
-		else {
-			printf( "%d is negative\n", n);
+		else
+	       	{
+			printf("%d is negative\n", n);
 		}
 		return (0);
 }

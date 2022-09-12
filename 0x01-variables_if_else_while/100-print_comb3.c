a72a0a9d756c13fb42998a75127a32c05345c34f
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-12 04:17:14 -0700

    D

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * print all possible duifferent combo of two digits
+ * prinit digit 1, digit 2
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = 0; digit2 < 10; digit++0)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

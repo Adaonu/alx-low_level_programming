2a8b01c03af50072737813f71b4d0df3d7e60ec4
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-12 02:37:00 -0700

    S

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * print all single digit numbers of base 10 
+ * starting from 0
+ * followed by a new line
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	 for (num = 0; num < 10; num++)
+	 {
+		 putchar((num % 10) + '0');
+	 }
+	 putchar('\n');
+		 return (0);
+}

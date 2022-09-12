76c70e22e4f7d4fe88c3404fbe201811ed4a263b
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-12 03:26:24 -0700

    S

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print all the numbers of base 16 in lowercase
+ * followed a newline
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for ( num = 0 ; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n')
+		return (0);
+}

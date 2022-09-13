4928316ed9e5624ccd8d71ea9f61ae038a09954d
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-13 07:08:51 -0700

    char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - entry point
+ * print _putchar, followed by a new line
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int _putchar;
+
+	for (_putchar = 0)
+	{
+		putchar(_putchar)
+	}
+
+	putchar('\n')
+			return 0
+}
+

b471f889c1391fc066a456b51aa14b8156342610
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-14 06:26:47 -0700

    S

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_to_98 - function that prints all natural numbers from n - 98
+ * @n: is the int that will use for argument gor the function
+ *
+ */
+void print_to_98
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+				printf("%d\n", n);
+	}
+}

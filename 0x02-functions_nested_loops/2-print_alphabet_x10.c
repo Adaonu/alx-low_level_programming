833f26378ca1632489470295008d186e3fea0656
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-14 02:26:50 -0700

    S

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@ void print_alphabet_x10(void)
 {
 	char letter, i;
 	
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (i = 0; i <= 9; i++)
+		
 	{
-		for (i = 0; i <= 9; i++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 		_putchar(letter);
 		}

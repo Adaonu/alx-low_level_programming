74844c204a0e9c636683ad2e824e5c5bf0f4ede6
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-14 02:45:15 -0700

    S

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _islower(int c) - function tht checks for lowercase character
+ *
+ * Return: 0 always
+ */
+int _islower(int c)
+{
+	if (c = 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+		return (0);
+}

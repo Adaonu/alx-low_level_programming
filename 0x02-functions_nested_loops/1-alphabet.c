ac4eeef3a11495c4a5f65cb25809be7e8e42dc30
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-14 01:46:49 -0700

    S

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ * write a founction that prints the alphabet
+ * in lowercase
+ * followed by a new line.
+ *
+ * Return: 0 always
+ */
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}

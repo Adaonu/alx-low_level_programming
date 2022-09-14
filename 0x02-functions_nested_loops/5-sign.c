c91ebf50aff256497e89f44c38f5781fc6cc25b2
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-14 03:35:56 -0700

    S

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main"
+/**
+ * print_sign - function that print the sign of a number
+ * @n: is the int tha will use for the argument in the function
+ *
+ * Return: 0 always
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	}
+	return (1);
+
+	else if (n = 0)
+	{
+		_putchar('0');
+	}
+	return (0);
+
+	else (n > 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}

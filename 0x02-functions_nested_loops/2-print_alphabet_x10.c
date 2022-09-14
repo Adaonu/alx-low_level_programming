dca7325d54ca3e0bd1eeecd05e69054d15681964
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-14 02:10:31 -0700

    S

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - fuction print 10 times the alphet
+ * in lowercase, followed by a new line.
+ *
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+		_putchar('10');
+	}
+	_putchar('\n');
+}

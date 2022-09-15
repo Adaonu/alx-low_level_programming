aaae4c668456be25b4935cedd3085128627bc91c
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-15 02:16:59 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print the first 50 number
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, c = 2;
+
+	while(i < 50)
+	{
+		if(i == 0)
+		{
+			printf("%ld", j);
+		}
+		else if (i== 1)
+		{
+			printf(", %ld", c);
+		}
+		else
+		{
+			c += j;
+			j = c - j;
+			printf(", %ld", c);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}

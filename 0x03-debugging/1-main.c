aa5cf995f0cf4b977ca1a917c4e2d49106077e31
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-15 04:56:02 -0700

    don't delete it!

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,15 +12,16 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*
 
 	*while (i < 10)
 	*{
-		putchar(i);
+        *putchar(i);
 	}*
 	*/
 
-	priintf("infinite loop avoided! \\o/\n");
+	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
 }

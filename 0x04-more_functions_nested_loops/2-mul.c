943e027baec0839def1e9df019d1b9c27b733b7e
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-16 03:40:47 -0700

    W

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * int mul - functions that multiples two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b)
+}

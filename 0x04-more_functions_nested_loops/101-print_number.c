d673cc4ca2c04e2d9b3050f1b976ff01b3cde919
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-16 10:08:56 -0700

    W

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,17 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
-	/*first check if its negative*/
+	unsigned int num  = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-	/*print the first few digits*/
 	if ((num / 10) > 0)
+	{
 		print_number(num / 10);
-	/*print the last digit*/
+	}
+
 	_putchar((num % 10) + 48);
 }

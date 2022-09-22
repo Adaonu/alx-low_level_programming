c26bd624fe85743ff1ad9597e4abd1d322db5457
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-22 01:43:21 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int 1;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-
 	for ( ; i < n; i++)
 	{
 		dest[i] = '\0';

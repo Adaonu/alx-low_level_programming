37786bbb47fad8bccc671aff452dec7b828dc7ef
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-28 08:37:33 -0700

    5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,38 @@
 #include  "main.h"
 /**
- *  _sqrt_recursion -  natural square root of a number.
+ *  power_operation -  natural square root of a number.
  *  @n: interger
+ *  @c: iterator
  *  Return: -1
  */
 
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+
+/**
+ *  _sqrt_recursion - returns the natural square root of a number
+ *  @n: input number
+ *  Return: square root
+ */
 
 int _sqrt_recursion(int n)
 {
-	if  (n => 0)
+	if  (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0)
+	if (n == 0)
+		return (0);
+	if (n == 1)
 		return (1);
 	return (_sqrt_recursion(n, 2));
 }

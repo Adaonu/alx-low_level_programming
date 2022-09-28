0d57fa81abbb020d36558fb161453ef841a67a72
Ada Onuorah <adaonuorah87@gmail.com>
2022-09-28 07:44:23 -0700

    0-puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _put_recursion - prints a string
+ * @string: string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s = 0)
+	{
+		_putchar(*s);
+		_putchar(s + 1)
+	}
+	else
+		_putchar('\n');
+}

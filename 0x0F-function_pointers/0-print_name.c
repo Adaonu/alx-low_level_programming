41131f7ab705d22e6cea196b28c7b618067ba85e
Ada Onuorah <adaonuorah87@gmail.com>
2022-10-12 04:35:52 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "fuction_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: input name
+ * @f: functions pointer
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name = &f)
+	{
+		f(name);
+	}
+}
